ca6957bd6c5d773488904dd3d3c38df26bdac96e
mathiasbritez <mathiasbritez2007@gmail.com>
2025-12-12 11:52:31 -0300

    fix vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ProyectoPF.BReactConAPI/",
-});
+  base: "./",     // 👈 ESTA LÍNEA ES LA QUE ARREGLA EL DEPLOY
+})
